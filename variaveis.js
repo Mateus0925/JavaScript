afc5f7d21f090c94f69f56d0f1fe822136900d71
Mateus Fernandes <josemateus0925@gmail.com>
2024-07-25 11:41:58 -0300

    first commit

---
+++ variaveis.js
@@ -0,0 +1,17 @@
+// Atividade - Calculo
+//Declarando as varáveis
+/*
+let nota01 = 8;
+let nota02 = 8;
+let nota03 = 8;
+let media = (nota01+nota02+nota03)/3;
+
+console.log("A média é: ", media);
+//adicionando comentário
+/*
+    Adicionar comentário
+*/
+
+const celsius = 25;
+const fahrenheit = (celsius * 9)/5 + 32;
+console.log("A tempertaratura em fahrenheit: ",fahrenheit);

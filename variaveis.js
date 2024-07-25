114a6365308694dda60584b62158c77a9c601ab3
Mateus Fernandes <josemateus0925@gmail.com>
2024-07-25 12:02:05 -0300

    segundo commit

--- variaveis.js
+++ variaveis.js
@@ -11,7 +11,28 @@ console.log("A média é: ", media);
 /*
     Adicionar comentário
 */
-
+/*
 const celsius = 25;
 const fahrenheit = (celsius * 9)/5 + 32;
 console.log("A tempertaratura em fahrenheit: ",fahrenheit);
+*/
+/*
+let minutos = 1;
+let segundos = minutos*60;
+console.log(`${minutos} minutos, convertendo para segundos fica: ${segundos} segundos`);
+*/
+let numero01 = 4;
+let numero02 = 5;
+
+const soma = numero01 + numero02;
+console.log('A soma é: ', soma);
+
+const subtracao = numero01 - numero02;
+console.log('A subtracao é: ', subtracao);
+
+const multiplicacao = numero01*numero02;
+console.log('A multiplicacao é: ', multiplicacao);
+
+const divisao = numero01 / numero02;
+console.log('A divisao é: ', divisao);
+
